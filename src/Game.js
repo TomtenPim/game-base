2d483a5a77961b7cdfef2fb6ff9697400c82a719
= <=>
2023-11-08 15:50:35 +0100

    fix

--- src/Game.js
+++ src/Game.js
@@ -23,8 +23,8 @@ export default class Game {
     this.enemyProjectiles = []
     this.enemyTimer = 0
     this.enemyInterval = 1000
-    this.enemyKilled = 0
-    this.mistMeter = 100
+    this.enemyKilled = =
+      this.mistMeter = 100
 
 
     this.player = new Player(this)
